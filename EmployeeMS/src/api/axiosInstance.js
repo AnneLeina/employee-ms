541825032a38a3c2f4ed649a0a4d3784d5d816b5
AnneLeina <117903163+AnneLeina@users.noreply.github.com>
2026-06-15 01:31:23 +0300

    Fix baseURL to ngrok

--- EmployeeMS/src/api/axiosInstance.js
+++ EmployeeMS/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import React from 'react'
 export const isDemoMode=!window.location.hostname.includes("localhost");
 
 const API= axios.create({
-    baseURL: isDemoMode ?"https://api-placeholder.com": " https://false-unshaved-lilac.ngrok-free.dev",
+    baseURL:  " https://false-unshaved-lilac.ngrok-free.dev",
     withCredentials:true
 })
   
